f47dbd6cfd35e4a2dadeee337987eac3e7c420c7
zoanana990 <harry021633@gmail.com>
2024-02-05 22:01:47 +0800

    Change the recursive method, use += instead of :=

--- 0.Makefile/mak/config.mk
+++ 0.Makefile/mak/config.mk
@@ -4,23 +4,26 @@
 # This file is used to project configure, include
 # qemu emulator board etc.
 ############################################################
-SYSTEM					:= $(shell pwd)
-SYSTEM_MAK				:= $(shell pwd)/mak
-SYSTEM_BUILD			:= $(SYSTEM)/build
+SYSTEM                  := $(shell pwd)
+SYSTEM_MAK              := $(shell pwd)/mak
+SYSTEM_BUILD            := $(SYSTEM)/build
 
-TARGET					:= ammaOS
-
-############################################################
-# include 				$(SYSTEM)/toolchain.mk
-include 				$(SYSTEM_MAK)/toolchain.mk
+TARGET                  := ammaOS
 
 ############################################################
-BOARD					:= raspi3b
-QEMU					:= qemu-system-aarch64
+# include                 $(SYSTEM)/toolchain.mk
+include                 $(SYSTEM_MAK)/toolchain.mk
 
-QFLAGS					:= -machine raspi3b -nographic
-DFLAGS					:=
+############################################################
+BOARD                   := raspi3b
+QEMU                    := qemu-system-aarch64
 
+QFLAGS                  := -machine raspi3b -nographic
+DFLAGS                  :=
+
+SRCS_TOTAL              :=
+OBJS_TOTAL              :=
+DEPS_TOTAL              :=
 ############################################################
 export SYSTEM_BUILD TARGET 
 export QFLAGS BOARD DFLAGS
