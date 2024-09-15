89c93cc0d3cc5a5ea1c8e01356ae8425359b735c
zoanana990 <harry021633@gmail.com>
2024-09-15 23:14:00 +0800

    add core directory

--- core/driver/uart-resberrypi3b.c
+++ core/driver/uart-resberrypi3b.c
@@ -1,6 +1,15 @@
-#include <amma/pl_uart.h>
 #include <amma/gpio.h>
 #include <arch/arm/io.h>
+
+#define U_BASE       (PBASE+0x00201000)
+
+#define U_DATA_REG   (U_BASE)
+#define U_FR_REG     (U_BASE+0x18)
+#define U_IBRD_REG   (U_BASE+0x24)
+#define U_FBRD_REG   (U_BASE+0x28)
+#define U_LCRH_REG   (U_BASE+0x2C)
+#define U_CR_REG     (U_BASE+0x30)
+#define U_IMSC_REG   (U_BASE+0x38)
 
 void uart_send(char c) {
     /* wait for transmit FIFO to have an available slot*/
